984be022a8364259342383e202ae24b0abec522a
Azat Sabirov <azatsabirov466@gmail.com>
2023-08-26 15:42:47 +0600

    set gradle dependencies

--- shared/src/commonMain/kotlin/com/asabirov/translator/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.asabirov.translator
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- shared/src/commonMain/kotlin/com/asabirov/translator/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.asabirov.translator
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
